7f8e2fa7e4bcb1699ddb5db8fdc7581ebc834765
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-02-28 20:11:24 -0500

    Fix spacing issues; correct operands; add Clear button functionality; and establish working happy path usage.

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ let operand1arr = [];
 let operand2arr = [];
 let userChoices = [];
 let operator = null;
+let operatorIndex = null;
 let operand1 = null;
 let operand2 = null;
+let resultNum = null;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -27,7 +29,7 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return (num1 / num2).toFixed(3) / 1;
 }
 
 function operate(operator, num1, num2 = num1) {
@@ -46,12 +48,12 @@ function operate(operator, num1, num2 = num1) {
 }
 
 function getOperands(userChoices) {
-    userChoices.pop();
-    let operatorIndex = null;
-    debugger;
+    if (userChoices[-1] == '=') {
+        userChoices.pop();
+    }
     for (let i = 0; i < userChoices.length; i++) {
         // if (/^[+\-x\/]&/.test(userChoices[i])) {
-            if (userChoices[i] == '+') {
+            if ((userChoices[i] == '+') || (userChoices[i] == '-') || (userChoices[i] == 'x') || (userChoices[i] == '/')) {
             operatorIndex = i;
             break;
             }
@@ -64,17 +66,31 @@ function getOperands(userChoices) {
     for (let i = operatorIndex + 1; i < userChoices.length; i++) {
         operand2arr.push(userChoices[i]);
     }
-    operand1 = parseInt(operand1arr.join(''));
-    operand2 = parseInt(operand2arr.join(''));
+    operand1 = parseFloat(operand1arr.join(''));
+    operand2 = parseFloat(operand2arr.join(''));
 }
 
 
 numBtns.forEach((button) => {
     button.addEventListener('click', function (e) {
         let num = e.target.innerText;
-        calculation.innerText += ` ${num}`;
+
+        if (result.innerText != '') {
+            clear();
+        }
+
+        calculation.textContent += `${num}`;
+
+        // if (calculation.innerText.includes(operator)) {
+        //     calculation.innerText += ` ${num}`;
+        // }
+        // else {
+        //     calculation.innerText += `${num}`;
+        // }
+        
         userChoices.push(num);
     });
+    
 });
 
 decBtn.addEventListener('click', function (e) {
@@ -84,15 +100,48 @@ decBtn.addEventListener('click', function (e) {
 opBtns.forEach((button) => {
     button.addEventListener('click', function (e) {
         operator = e.target.innerText;
-        calculation.innerText += ` ${operator}`;
+        calculation.textContent += ` ${operator} `;
+        if (operatorIndex != null) {
+            calculation.textContent = `${resultNum} ${operator} `;
+            result.textContent = '';
+        };
     });
 });
 
-eqBtn.addEventListener('click', function (e) {
-    calculation.innerText += ` ${e.target.innerText}`;
+eqBtn.addEventListener('click', function () {
+    equalsJobs();
+});
+
+function equalsJobs() {
+    if (calculation.innerText.includes('=')) {
+        calculation.innerText = `${calculation.innerText.slice(0, -2)}`;
+    };
+    calculation.innerText += ` =`;
+    debugger;
     getOperands(calculation.innerText.replace(/\s+/g,'').split(''));
-    result.innerText = operate(operator, operand1, operand2);
+    resultNum = operate(operator, operand1, operand2);
+    result.innerText = resultNum;
+    operand1arr = [];
+    operand2arr = [];
+    userChoices = [];
+}
+
+clrBtn.addEventListener('click', function() {
+    clear();
+})
+
+function clear () {
+    calculation.innerText = '';
+    result.innerText = '';
     for (let i = 0; i < operand1arr.length; i++) {
         operand1arr.pop();
     };
-});
+    for (let i = 0; i< operand2arr.length; i++) {
+        operand2arr.pop();
+    };
+    for (let i = 0; i < userChoices.length; i++) {
+        userChoices.pop();
+    }
+    operator = null;
+    operatorIndex = null;
+}
